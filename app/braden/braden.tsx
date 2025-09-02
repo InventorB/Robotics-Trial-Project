f6c7a884121823a9178f020347fa2dae2bc6c696
Braden Wagner <bradenwagner06@gmail.com>
2025-09-01 23:10:22 -0700

    Fullscreen icon

--- app/braden/braden.tsx
+++ app/braden/braden.tsx
@@ -1,4 +1,10 @@
 
+import { Welcome } from "../welcome/welcome";
+
+// fullscreen icons
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
+
 
 export function Main() {
   return (
@@ -14,6 +20,7 @@ export function Topbar() {
     return(
     <div className= "bg-green-300 p-3" >
         <a className = "text-gray-900 text-lg" href="#home">Home</a>
+        <FullscreenIcon className="float-right bg-gray-900 rounded-lg"/>
     </div>
     );
 }
