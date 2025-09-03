e7019f16e51d32a72eae7d5ab378c04df56c044c
Braden Wagner <bradenwagner06@gmail.com>
2025-09-03 00:12:33 -0700

    Video player padding improvements

--- app/braden/body.tsx
+++ app/braden/body.tsx
@@ -6,9 +6,6 @@ export function Body() {
     )
 }
 
-
-
-
 export function Braden() {
     return (
     <div className="flex items-center justify-center pt-3 pb-6 text-3xl">
@@ -18,7 +15,9 @@ export function Braden() {
 }
 export function Video() {
     return(
+        <div className="py=2"> 
         <iframe width="1100" height="600" src="https://www.youtube.com/embed/4TIpitPIsPA?si=VJkDSUyuFRcKcYtO?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
     )
 }
 
