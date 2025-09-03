d76f6b45150c3a7fd8d97b22710f7a9a24bb9870
Braden Wagner <bradenwagner06@gmail.com>
2025-09-03 00:17:37 -0700

    Youtube allow fullscreen

--- app/braden/body.tsx
+++ app/braden/body.tsx
@@ -16,7 +16,7 @@ export function Braden() {
 export function Video() {
     return(
         <div className="py=2"> 
-        <iframe width="1100" height="600" src="https://www.youtube.com/embed/4TIpitPIsPA?si=VJkDSUyuFRcKcYtO?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="1100" height="600" src="https://www.youtube.com/embed/4TIpitPIsPA?si=VJkDSUyuFRcKcYtO?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     )
 }
