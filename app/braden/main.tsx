a3d3ee697aa1563ffe520e3454085897c6c5cb4f
Braden Wagner <bradenwagner06@gmail.com>
2025-09-07 15:11:41 -0700

    React hook for fullscreen button

--- app/braden/main.tsx
+++ app/braden/main.tsx
@@ -1,10 +1,13 @@
+// images
 import FullscreenIcon from "@mui/icons-material/Fullscreen";
 import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/DarkMode";
-
+// previos files
 import { Welcome } from "../welcome/welcome";
 import { Body } from "../braden/body";
+// react hooks
+import React, { useState, useEffect } from "react";
 
 
 
@@ -23,10 +26,11 @@ export function Main() {
 
 export function Topbar() {
     return(
-    <div className= "bg-blue-700 p-3" >
+    <div className= "bg-blue-700 p-3 flex items-center" >
         <a className = "text-gray-900 text-lg text-white" href="#home">Home</a>
-        <FullscreenIcon className="float-right bg-gray-900 rounded-lg"/>
-        <DarkModeIcon className="float-right bg-gray-900 rounded-lg mx-2"/>
+        <div className="ml-auto flex items-center gap-3">
+          <FullscreenButton />
+        </div >
     </div>
     );
 }
@@ -38,5 +42,25 @@ export function BottomBar() {
         <a className = "text-gray-900 ml-auto px-2 text-lg" >Copyright Braden Wagner 2025</a>
     </div>
     );
+}
+
+export function FullscreenButton() {
+    const [isFullScreen, setFullScreen] = useState(false);
+    
+  const toggleFullscreen = () => {
+    console.log("Toggling fullscreen.");
+    if (!isFullScreen) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+  setFullScreen(!isFullScreen);
+};
+  
+  return(
+  <button onClick={toggleFullscreen}>
+  <FullscreenIcon className="inline-flex"/>
+  </button>
+  )
 }
 
