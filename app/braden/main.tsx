d3ac7d7dff444720a5708b933659f22a11a48756
Braden Wagner <bradenwagner06@gmail.com>
2025-09-07 19:04:04 -0700

    Update wording

--- app/braden/main.tsx
+++ app/braden/main.tsx
@@ -27,7 +27,7 @@ export function Main() {
 export function Topbar() {
     return(
     <div className= "bg-blue-700 p-3 flex items-center" >
-        <a className = "text-gray-900 text-lg text-white" href="#home">Home</a>
+        <a className = "text-gray-900 text-lg text-white" href="#home">Mission Control Home</a>
         <div className="ml-auto flex items-center gap-3">
           <FullscreenButton />
         </div >
@@ -37,9 +37,9 @@ export function Topbar() {
 export function BottomBar() {
     return(
     <div className= "fixed bottom-0 left-0 w-full bg-orange-500 p-3 flex items-center" >
-        <a className = "text-gray-900 text-lg" href="#home">Server Online</a>
-        <a className = "text-gray-900 text-lg px-2 space-x-4" >23ms</a>
-        <a className = "text-gray-900 ml-auto px-2 text-lg" >Copyright Braden Wagner 2025</a>
+        <a className = "text-gray-900 text-lg" href="#home">Connected</a>
+        <a className = "text-gray-900 text-lg px-2 space-x-4" ></a>
+        <a className = "text-gray-900 ml-auto px-2 text-lg" >Robotics Trial Project 2025</a>
     </div>
     );
 }
