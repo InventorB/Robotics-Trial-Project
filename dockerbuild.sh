cec101489c4c9d910db50f3cf4f8e41762d3e2e3
Braden Wagner <bradenwagner06@gmail.com>
2025-09-13 14:52:19 -0700

    fix docker build command

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,9 @@
+# build into local docker
 docker buildx build \
   --platform=linux/amd64 \
   -t robot-trial \
-  --output type=tar,dest=robot-trial.tar \
+  --load \
   .
+
+# now export in the right format
+docker save robot-trial -o robot-trial.tar
